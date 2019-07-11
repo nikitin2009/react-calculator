2811a81bc1906c9d86bb3d440d797267ba529236
Alex N <nikitin2009@gmail.com>
2019-07-11 09:44:53 +0300

    Updates

---
+++ precache-manifest.39887716b280853e62d2d487e4fbf373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85544bd8fdc7138255b3836de419db6",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "4f727c9608b1c8d5e95d",
+    "url": "/react-calculator/static/css/main.36b16ea1.chunk.css"
+  },
+  {
+    "revision": "809458f1ccadb754d6b1",
+    "url": "/react-calculator/static/js/2.bd16afac.chunk.js"
+  },
+  {
+    "revision": "4f727c9608b1c8d5e95d",
+    "url": "/react-calculator/static/js/main.7c90cc97.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
